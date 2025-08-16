6743b74fe98cacdb1e64a144f0dd2ceb96109044
ak5028 <aj4467507@gmail.com>
2025-08-16 14:09:34 +0530

    Create 8.Transform the Expression.cpp
    https://www.spoj.com/problems/ONP/

---
+++ 8.Transform the Expression.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ 
+//no need of any order priority
+int main(){
+      int t;cin>>t;
+      while(t--){
+        string s;cin>>s;
+        stack<int>st;
+        for(char ch:s){
+                if(ch=='(' || ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^'){st.push(ch);}
+                else if(ch==')'){
+                        while(true){
+                                char c=st.top();
+                                st.pop();
+                                if(c=='('){break;}
+                                else{cout<<c;}
+                        }
+                }
+                else{cout<<ch;}
+       }cout<<endl;
+     }           
+} 
