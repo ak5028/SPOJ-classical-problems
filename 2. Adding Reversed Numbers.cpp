f6ab1cd08019dedb93c73fa354dbce68c459ced3
ak5028 <aj4467507@gmail.com>
2025-08-17 14:45:08 +0530

    Create 2. Adding Reversed Numbers.cpp(https://www.spoj.com/problems/ADDREV/)

---
+++ 2. Adding Reversed Numbers.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ 
+int main(){
+      int t;cin>>t;
+      while(t--){
+        string a,b;cin>>a>>b;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int c=stoi(a)+stoi(b);
+        string c1=to_string(c);
+        int n=c1.size(),x=0;
+        for(int i=n-1;i>=0;i--){
+                if(c1[i]=='0' && x==0){continue;}
+                else{x=1;cout<<c1[i];}    
+        }
+        cout<<endl;
+ 
+     }           
+} 
