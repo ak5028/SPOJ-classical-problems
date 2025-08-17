9e323f83f30b54d7eef72d3672c191206ea49c37
ak5028 <aj4467507@gmail.com>
2025-08-17 14:49:41 +0530

    Create 4. Factorial.cpp(https://www.spoj.com/problems/FCTRL/)

---
+++ 4. Factorial.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ 
+int main(){
+      int t;cin>>t;
+      while(t--){
+        int n;cin>>n;int ans=0;
+        while(n!=0){
+                ans+=n/5;
+                n/=5;
+        }
+        cout<<ans<<endl;
+     }           
+} 
