f645f61a961163725fe40e4929775a2863d0ea90
ak5028 <aj4467507@gmail.com>
2025-08-17 14:56:03 +0530

    Update 1. Life, the Universe, and Everything.cpp(https://www.spoj.com/problems/TEST/)

--- 1. Life, the Universe, and Everything.cpp
+++ 1. Life, the Universe, and Everything.cpp
@@ -5,7 +5,7 @@ typedef long long  ll;
 int main(){
       vector<int>v;
       while(true){
-        int n;cin>>n;
+        int n; cin>>n;
         if(n==42){break;}
         v.push_back(n);
       }
